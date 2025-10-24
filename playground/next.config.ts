2684a385102754373aa4e9d00080aaffa9d057ee
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-10-24 10:25:16 +0600

    chore(playground): upgrade to deps to latest

--- playground/next.config.ts
+++ playground/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? undefined,
+  turbopack: {
+    root: path.join(__dirname, '../')
+  }
 };
 
 export default nextConfig;
